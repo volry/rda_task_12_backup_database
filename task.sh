85c7965559e0ec38079634477b18fe420fba27e3
volry <v.v.ryzhuk@gmail.com>
2024-08-15 18:17:47 +0300

    task.sh ready

--- task.sh
+++ task.sh
@@ -1,1 +1,13 @@
 #! /bin/bash
+
+# Here I read database credentials
+DB_USER="${DB_USER}"
+DB_PASSWORD="${DB_PASSWORD}"
+
+# Backup ShopDB 
+mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB | mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve
+
+# Dump from ShopDB and restore ShopDBDevelopment
+mysqldump -u $DB_USER -p$DB_PASSWORD --no-create-info ShopDB | mysql -u $DB_USER -p$DB_PASSWORD ShopDBDevelopment
+
+echo "Backup done well ))."
